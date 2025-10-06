ca882444e9f07b579472a711b615ed79a8ee3c67
OMulderij <owenmulderij1@gmail.com>
2025-10-06 13:42:39 +0200

    Added a third person camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,21 +17,19 @@ public class PlayerMovement : MonoBehaviour
     public Camera playerCamera;
     public float walkSpeed = 6f;
     public float runSpeed = 12f;
-    public float jumpPower = 7f;
-    public float gravity = 10f;
-    public float lookSpeed = 2f;
-    public float lookXLimit = 45f;
+    public float jumpPower = 10f;
+    public float gravity = 15f;
+    public float lookSpeed = 200f;
+    public float lookXLimit = 75f;
     public float defaultHeight = 2f;
-    public float crouchHeight = 1f;
-    public float crouchSpeed = 3f;
-    public float coyoteTime = 0.5f;
+    public float bufferTime = 0.25f;
     public LayerMask groundMask;
 
     private Vector3 moveDirection = Vector3.zero;
     private Vector3 momentumVar;
-    private float rotationX = 0;
+    private float pitch = 0;
+    private float yaw = 0;
     private CharacterController characterController;
-    private Transform playerTransform;
 
     private bool canMove = true;
     private bool canJump = true;
@@ -57,7 +55,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void CheckForMoveAndJump()
     {
-        bool isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, characterController.height / 2, 0), 0.45f, groundMask);
+        bool isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, characterController.height / 2, 0), 0.2f, groundMask);
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
@@ -79,11 +77,11 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = (forward * moveValue.y) + (right * moveValue.x);
         if (!isGrounded && moveAction.IsPressed())
         {
-            momentumVar.x = Mathf.Lerp(momentumVar.x, moveDirection.x, 0.005f);
-            momentumVar.z = Mathf.Lerp(momentumVar.z, moveDirection.z, 0.005f);
+            momentumVar.x = Mathf.Lerp(momentumVar.x, moveDirection.x, 2f * Time.deltaTime);
+            momentumVar.z = Mathf.Lerp(momentumVar.z, moveDirection.z, 2f * Time.deltaTime);
         }
 
-        if (jumpAction.IsPressed() && canMove && canJump && Time.time - timeWhenLastGrounded < coyoteTime)
+        if (jumpAction.IsPressed() && canMove && canJump && Time.time - timeWhenLastGrounded < bufferTime)
         {
             if (isGrounded)
             {
@@ -115,11 +113,25 @@ public class PlayerMovement : MonoBehaviour
     {
         if (canMove)
         {
-            Vector2 mouseMovement = lookAction.ReadValue<Vector2>() / 10;
-            rotationX += -mouseMovement.y * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, mouseMovement.x * lookSpeed, 0);
+            Vector2 mouseMovement = lookAction.ReadValue<Vector2>() * 10;
+            yaw += mouseMovement.x * lookSpeed * Time.deltaTime;
+            pitch -= mouseMovement.y * lookSpeed * Time.deltaTime;
+
+            pitch = Mathf.Clamp(pitch, -lookXLimit, lookXLimit);
+
+            Quaternion rotation = Quaternion.Euler(pitch, yaw, 0f);
+
+            Vector3 offset = rotation * new Vector3(0f, 0f, -5.0f);
+            playerCamera.transform.position = transform.position + offset;
+
+            Vector3 camForward = playerCamera.transform.forward;
+            camForward.y = 0f;
+
+            Quaternion targetRotation = Quaternion.LookRotation(camForward);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * 10f);
+
+            Vector3 pointToLookAt = new Vector3(0, 0, 0);
+            playerCamera.transform.LookAt(this.transform.position + pointToLookAt);
         }
     }
     public void OnControllerColliderHit(ControllerColliderHit hit)
