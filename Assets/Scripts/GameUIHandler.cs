d069c3f254767953b7a027fa9930f397f204a578
OMulderij <owenmulderij1@gmail.com>
2025-10-28 10:04:54 +0100

    Finished work on the Mana bar functionality

--- Assets/Scripts/GameUIHandler.cs
+++ Assets/Scripts/GameUIHandler.cs
@@ -7,12 +7,15 @@ public class GameUIHandler : MonoBehaviour
     public PlayerControl PlayerControl;
     private UIDocument UIDoc;
     private Label m_HealthLabel;
+    private VisualElement m_HealthBarMask;
+    
 
 
     private void Start()
     {
         UIDoc = GetComponent<UIDocument>();
         m_HealthLabel = UIDoc.rootVisualElement.Q<Label>("ManaLabel");
+        m_HealthBarMask = UIDoc.rootVisualElement.Q<VisualElement>("ManaBarMask");
         PlayerControl.OnHealthChange += HealthChanged;
         HealthChanged();
     }
@@ -21,5 +24,8 @@ public class GameUIHandler : MonoBehaviour
     void HealthChanged()
     {
         m_HealthLabel.text = $"{Math.Round(PlayerControl.currentHealth)}/{PlayerControl.maxHealth}";
+        float healthRatio = (float)PlayerControl.currentHealth / PlayerControl.maxHealth;
+        float healthPercent = Mathf.Lerp(8, 88, healthRatio);
+        m_HealthBarMask.style.width = Length.Percent(healthPercent);
     }
 }
