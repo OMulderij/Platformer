f15a8c9c2d1a88ae1c6a1cb85895acec579994d7
OMulderij <owenmulderij1@gmail.com>
2025-10-31 11:36:59 +0100

    Added an animation and particle system to the pillar spawning in and getting destroyed.

--- Assets/Scripts/AbilityPlatform.cs
+++ Assets/Scripts/AbilityPlatform.cs
@@ -1,23 +1,43 @@
+using System;
 using UnityEngine;
 
 public class AbilityPlatform : MonoBehaviour
 {
     public float lifetime = 10f;
-    public float animationLength = 1f;
+    public float animationLength = 0.5f;
+    public GameObject platform;
+    public ParticleSystem spawnParticles;
+    public ParticleSystem destroyParticles;
     private float timeWhenSpawned = 0.0f;
-    private bool spawned = false;
+    private bool animationPlayed = false;
 
     public void StartTimer()
     {
         timeWhenSpawned = Time.time;
-        spawned = true;
+        this.transform.localScale = new Vector3(1, 1, 0);
+
+        spawnParticles.Play();
+
+        Destroy(platform, lifetime);
+        Destroy(gameObject, lifetime + animationLength);
     }
 
     public void FixedUpdate()
     {
-        if (spawned && Time.time - timeWhenSpawned > lifetime)
+        if (Time.time - timeWhenSpawned < animationLength)
         {
-            Destroy(gameObject);
+            Vector3 currentScale = new Vector3(1, 1, Mathf.Lerp(0, 1, (Time.time - timeWhenSpawned) / animationLength));
+            this.transform.localScale = currentScale;
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (!animationPlayed && Time.time - timeWhenSpawned > lifetime - animationLength / 2)
+        {
+            destroyParticles.Play();
+            animationPlayed = true;
         }
     }
 }
